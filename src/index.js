22f49cf9c4429a3f349af2e359b6a493c2561d93
lanasiqueira <helanasiqueira7@gmail.com>
2026-06-01 21:44:28 -0300

    meu novo commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import router from "./routes.js";
+import router from './routes.ts';
 
 const app = express()
 
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
+
 app.listen(3001, () => {
-  console.log('Servidor executando na porta: 3001')
+  console.log('Servidor executando na parta: http://localhost:3001')
 })
