e3bb42fbab443ef931a4f8908e4d509d508fbefd
lanasiqueira <helanasiqueira7@gmail.com>
2026-06-03 21:44:51 -0300

    commit 03/06

---
+++ src/services/servCategoria.js
@@ -0,0 +1,9 @@
+import repositoryCategoria from "../repositories/repoCategoria.js";
+
+async function Listar() {
+
+    const categorias = await repositoryCategoria.Listar();
+    return categorias;
+}
+
+export default { Listar };
